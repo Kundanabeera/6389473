880c6189e1ce1f2fb7b8518174a6b781850fcaf8
Kundanabeera <beerakundana@gmail.com>
2025-08-04 12:20:58 +0530

    Add files via upload

---
+++ CDN/week-7/12. ReactJS-HOL/ticketbookingapp/src/GuestPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function GuestPage() {
+  return (
+    <div>
+      <h2>Flight Details</h2>
+      <p>Flight: AI-202 | From: Delhi | To: Mumbai | Time: 10:00 AM</p>
+      <p>Flight: AI-305 | From: Chennai | To: Bangalore | Time: 12:30 PM</p>
+      <p>Please login to book tickets.</p>
+    </div>
+  );
+}
+
+export default GuestPage;
